5f2b4eee0551128ad39cc52e59dcf7d50e4bb25b
Ryan <Ryan@10.1.9.233>
2013-01-31 04:20:12 -0600

    First commit

---
+++ src/StringLength.java
@@ -0,0 +1,26 @@
+
+/** This program demonstrates how to use the String class's length method
+ * 
+ * @author Ryan Hickman
+ *
+ */
+
+public class StringLength 
+{
+	public static void main (String [] args)
+	{
+		
+		String name = "Herman";	// Assigns the name "Herman" to the String variable name
+		int stringSize;			// Creates the stringSize variable
+		
+		stringSize = name.length();	// Calculates the name variable's length
+		
+		// Displays the message and the character length of Herman
+		System.out.println(name + " has " + stringSize + " characters");
+		
+		
+		
+		
+	}
+
+}
